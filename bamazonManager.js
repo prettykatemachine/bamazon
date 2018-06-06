4e5bc506d18a51e9b95c1687c45e31d3b771dde6
unknown <katie.albee@gmail.com>
2018-06-05 20:43:54 -0500

    javascripty

--- bamazonManager.js
+++ bamazonManager.js
@@ -70,3 +70,141 @@ function printTable(res) {
   }
   console.table(headings, items);
 }
+function showItems() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		console.log("\nAll Products\n");
+		printTable(res);
+		setTimeout(menu, 1000);
+	});
+}
+function menu() {
+	inq.prompt(
+		{
+			name: "menu",
+			type: "confirm",
+			message: "Enter Manager Portal"
+		}
+	).then(function(answer) {
+		if (answer.menu) {
+			prompt();
+		} else {
+			exit();
+		}
+	});
+}
+function lowInv() {
+	connection.query("SELECT * FROM products WHERE stock_quantity < 20", function(err, res) {
+		if (err) throw err;
+		if (res.length === 0) {
+			console.log("\nThere are no items low in stock.\n");
+		} else {
+			console.log("\nLow Inventory\n");
+			printTable(res);
+			setTimeout(menu, 1000);
+		}
+	});
+}
+function addInv() {
+	var selected;
+
+	inq.prompt([
+		{
+			name: "itemID",
+			type: "input",
+			message: "Please enter the ID number of the item you'd like to update.",
+			validate: function(value) {
+				if (value <= 0 || isNaN(value)) {
+					console.log("\nPlease enter a valid item ID.\n");
+				} else {
+					return true;
+				}
+			}
+		},
+		{
+			name: "quantity",
+			type: "input",
+			message: "How many units of this item would you like to add?",
+			validate: function(value) {
+				if (value <= 0 || isNaN(value)) {
+					console.log("\nPlease enter a valid number.\n");
+				} else {
+					return true;
+				}
+			}
+		}
+	]).then(function(answer) {
+		connection.query("SELECT * FROM products WHERE item_id = ?",[answer.itemID], function(err, res) {
+			if (err) throw err;
+			selected = res[0]; 
+		});
+
+		connection.query("UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?", [answer.quantity, answer.itemID], function(err, res) {
+			if (err) throw err;
+			console.log("\nThe inventory has been successfully updated!\n");
+
+			inq.prompt(
+				{
+					name: "addAnother",
+					type: "confirm",
+					message: "Would you like to update another item?"
+				}
+			).then(function(answer) {
+				if (answer.addAnother) {
+					addInv();
+				} else {
+					menu();
+				}
+			});
+		});
+	});
+}
+
+function newProduct() {
+	inq.prompt([
+		{
+			name: "name",
+			type: "input",
+			message: "Please enter the product name:"
+		},
+		{
+			name: "dept",
+			type: "input",
+			message: "Please enter the department name:"
+		},
+		{
+			name: "price",
+			type: "input",
+			message: "Please enter the product's price:"
+		},
+		{
+			name: "quantity",
+			type: "input",
+			message: "Please enter the quantity of the product:"
+		}
+	]).then(function(answer) {
+		connection.query("INSERT INTO products SET ?",
+		{
+			product_name: answer.name,
+			department_name: answer.dept,
+			price: answer.price,
+			stock_quantity: answer.quantity
+		},
+		function(err, res) {
+			if (err) throw err;
+			var message = "\n" + answer.name + " was successfully added to the inventory!\n";
+			console.log(message);
+			menu();
+		});
+	});
+}
+
+
+function exit() {
+	console.log("\nSession ended.\n");
+	connection.end();
+}
+
+
+
+
